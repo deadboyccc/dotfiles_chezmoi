e1c2159aca583d4ad587a996be9ca6f5d2dacfd1
adastra <ahmedmohammed199975@gmail.com>
2024-03-26 14:04:46 +0300

    x11 stuff + dmenu + wezterm

---
+++ dot_wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+-- config.color_scheme = "AdventureTime"
+-- config.color_scheme = "tokyonight_night"
+-- config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Chalk"
+config.window_background_opacity = 0.8
+config.enable_tab_bar = false
+config.font_size = 15.5
+return config
